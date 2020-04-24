b422070b2192234d67178531d3a44b54f2d3a476
Shivakumara Bannimatti <shivaslb72@gmail.com>
2020-04-24 09:39:04 +0530

    Updates

---
+++ precache-manifest.0a1a65f1223fc1333090b349837db6fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426eaa3c7e8217e762163f7a69554a86",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "0319eb2dbc02fe564614",
+    "url": "/react-gh-pages/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d6dc27a559634f24c89f",
+    "url": "/react-gh-pages/static/css/main.e7245f43.chunk.css"
+  },
+  {
+    "revision": "0319eb2dbc02fe564614",
+    "url": "/react-gh-pages/static/js/2.21401d90.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/react-gh-pages/static/js/2.21401d90.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6dc27a559634f24c89f",
+    "url": "/react-gh-pages/static/js/main.60d1474b.chunk.js"
+  },
+  {
+    "revision": "8a312cabc2fdf1546ed0",
+    "url": "/react-gh-pages/static/js/runtime-main.7be909b7.js"
+  },
+  {
+    "revision": "cb800e770af2a6800b26719a854e3abc",
+    "url": "/react-gh-pages/static/media/img1.cb800e77.jpeg"
+  }
+]);
